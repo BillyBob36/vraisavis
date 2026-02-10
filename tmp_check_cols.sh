bb1b6efd41cd451e48c58f9c76c081a745a43468
BillyBob36 <105729899+BillyBob36@users.noreply.github.com>
2026-02-10 12:50:44 +0100

    chore: remove temp debug scripts

--- tmp_check_cols.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-DB_CONTAINER="x4osww00skows0ck4g0k4ogo"
-echo "=== All vendor columns ==="
-docker exec "$DB_CONTAINER" psql -U vraisavis -d vraisavis -c "SELECT column_name, data_type FROM information_schema.columns WHERE table_name='vendors' ORDER BY ordinal_position;"
